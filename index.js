2cbf594c7ca53e9181aea83226cc7ea729790229
minaseg <20640310001@ogrenci.bartin.edu.tr>
2024-06-01 16:33:23 +0300

    change port

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/category/:id', (req, res) => {
     .catch(err => console.error(err));
 })
 
-app.listen(8080, () => {
+app.listen(3000, () => {
       console.log('server listening on port 8080')
 })
 
